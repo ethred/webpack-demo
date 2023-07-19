419bea0f59beec6fa0d60fda9c817d0340daf170
redwan <redwanthr@gmail.com>
2023-07-19 07:39:00 -0700

    update index.html

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function component() {
   
      // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('Hello');
+    element.classList.add('hello');
   
     return element;
   }
